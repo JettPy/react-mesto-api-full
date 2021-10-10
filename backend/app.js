beae0106fd373cb3b747b9a5643a9220c6352569
Sergey Melnikov <jettpy@yandex.ru>
2021-10-10 16:51:33 +0300

    fix: тест корса

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,16 @@ const auth = require('./middlewares/auth');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(cors({
+  credentials: true,
+}));
 app.use(helmet());
 app.disable('x-powered-by');
 app.use(bodyParser.json());
